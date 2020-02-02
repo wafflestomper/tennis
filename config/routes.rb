5ed34c3fcc64906995abb6f15c0a5ddd92781739
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2020-02-02 15:38:24 -0500

    deleting welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/Index'
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
