8ac8d0f45e9865c8cdb90a9492cbfdb869fe694c
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2020-02-02 23:24:51 -0500

    added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
+  devise_for :users
   root 'welcome#index'
+  get 'welcome/about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
