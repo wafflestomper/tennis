daa5155910ae67a6337b83b9e64f5171db89b596
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2020-02-06 15:19:22 -0500

    added site links to bottom of logged in page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,7 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def account
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
-  get 'welcome/about'
+  root "welcome#index"
+  get "welcome/about"
+  get "welcome/account"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
