14c82fec6919a512e8ef66fab74a7dc88e29e0b2
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2020-02-15 15:23:11 -0500

    deleting x-- files

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,7 @@
-class Match < ApplicationRecord 
+class Match < ApplicationRecord
   validates :opponent_name, presence: true
   validates :opponent_score, presence: true
 
+  #belongs_to :user
 
 end
